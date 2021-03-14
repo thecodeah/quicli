a8468b31bdf76b26513154e907b16040b7574836
thecodeah <codeah@protonmail.com>
2021-03-15 00:48:47 +0100

    chore: Updated bump script to commit CHANGELOG

--- scripts/bump.js
+++ scripts/bump.js
@@ -1,5 +1,6 @@
 const { execSync } = require('child_process');
-const git = require("simple-git");
+const simpleGit = require("simple-git");
+const git = simpleGit();
 
 const validArgs = ["major", "minor", "patch"];
 const args = process.argv.slice(2);
@@ -13,16 +14,18 @@ if (args.length > 0) {
 }
 
 function bump(version) {
-    git().status().then((status) => {
+    git.status().then((status) => {
         if (status.files.length === 0) {
             execSync(`npm version ${version}`); // Bumps the version in package.json
             execSync(`npx changelog`); // Generate the changelog
             execSync("npm run build"); // Rebuild the project to update the version in the header of quicli.min.js
 
+            git.add(["CHANGELOG.md"]).commit("Updated CHANGELOG.md");
+
             // You might not want to commit/push/publish straight away, so provide the commands to do it.
             console.log("You might want to run these commands:");
-            console.log(` > git add . && git commit -m 'Bumped ${version} version'`);
-            console.log(" > git push origin master && npm publish");
+            console.log(" > git push origin master && git push --tags");
+            console.log(" > npm publish");
         } else {
             console.log("Commit your changes first!");
         }
